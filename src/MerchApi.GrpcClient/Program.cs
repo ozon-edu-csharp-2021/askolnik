b1df9252a14c1b2fd2f5338fbd668a9e0b1560cc
alex-fbr <alex-fbr@yandex.ru>
2021-10-24 14:52:53 +0300

    Add gRPC

---
+++ src/MerchApi.GrpcClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Grpc.Core;
+using Grpc.Net.Client;
+
+using MerchApi.Grpc;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new MerchApiGrpc.MerchApiGrpcClient(channel);
+
+try
+{
+    var response = await client.GetMerchPackAsync(new GetMerchPackRequest
+    {
+        Id = 1,
+    });
+
+    Console.WriteLine(response?.MerchPack);
+}
+catch (RpcException e)
+{
+    Console.WriteLine(e);
+}
+
+
+try
+{
+    var response = await client.GetMerchDeliveryInfoAsync(new GetMerchDeliveryInfoRequest
+    {
+        Id = 1
+    });
+
+    Console.WriteLine(response?.MerchDeliveryInfo);
+}
+catch (RpcException e)
+{
+    Console.WriteLine(e);
+}
+
+Console.ReadKey();
